f16a482d58da0719bca081d15e20301e50d3f128
Kenny Leong <kennyleong2@gmail.com>
2023-04-29 21:26:32 -0400

    fixed scrolling on sent/received message

--- react-app/src/components/ChannelMessages/index.js
+++ react-app/src/components/ChannelMessages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import MessageItem from "../MessageItem";
 import "./ChannelMessages.css";
@@ -6,6 +6,42 @@ import "./ChannelMessages.css";
 
 function ChannelMessages({ messages }) {
     const channel = useSelector(state => state.channels.oneChannel)
+    const scrollerRef = useRef(null);
+    const messageListRef = useRef(null);
+
+
+    // Scroll to bottom of message list when it's first rendered
+    useEffect(() => {
+        if (messageListRef.current) {
+            messageListRef.current.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, []);
+
+    // Scroll to bottom of message list when a new message is received or sent
+    useEffect(() => {
+        messageListRef.current.scrollIntoView({ behavior: 'smooth' });
+
+    }, [messages]);
+
+
+    if (!messages) {
+        return (
+            <div className='loading-animation'>
+                <div className="center">
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className='channel-messages-container'>
@@ -16,13 +52,13 @@ function ChannelMessages({ messages }) {
                 <h2 className="channel-messages-welcome">Welcome to #{channel.name}!</h2>
                 <p className="channel-messages-start">This is the start of the #{channel.name} channel.</p>
             </div>
-            <div id='scroller'>
+            <div className="channel-messages-section">
                 {messages.map(message => (
                     <div key={`message${message.id}`} className='message-item-container'>
                         <MessageItem message={message} />
                     </div>
                 ))}
-                <div id='anchor'></div>
+                <div ref={messageListRef} />
             </div>
         </div>
     );
