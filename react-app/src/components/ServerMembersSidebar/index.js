35c24813533aaea3e4df245f07a91e07de8c7938
Kenny Leong <kennyleong2@gmail.com>
2023-05-01 21:51:23 -0400

    working on server sidebar

--- react-app/src/components/ServerMembersSidebar/index.js
+++ react-app/src/components/ServerMembersSidebar/index.js
@@ -1,18 +1,107 @@
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import OpenModalButton from "../OpenModalButton";
 import ServerMemberAdd from "../ServerMemberAdd";
+import { useHistory } from "react-router-dom";
 import { getServer, deleteServerMember } from "../../store/server";
+import { getUserFriends, getNonFriends, getFriendRequests } from "../../store/friends";
+import { getUserServers, addPrivateServer, getFriendServers } from "../../store/server";
+import logo from '../../static/phantasmal-logo-trans.png';
+import { useModal } from "../../context/Modal";
+import { io } from 'socket.io-client';
 import "./ServerMemberSidebar.css";
+
+
+let socket;
+
 
 const ServerMembersSidebar = () => {
     const dispatch = useDispatch();
-    let user = useSelector(state => state.session.user);
-    let server = useSelector(state => state.server.currentServer);
-    let members = server.members;
+    const history = useHistory();
 
-    const handleDM = (e) => {
-        e.preventDefault();
-        window.alert('Private Messages Feature Coming Soon!');
+    const { setModalContent } = useModal();
+
+    const currentUser = useSelector(state => state.session.user);
+    const server = useSelector(state => state.server.currentServer);
+    const userServers = useSelector(state => state.server.userServers)
+    const userFriends = useSelector(state => state.friends.userFriends)
+    const members = server.members;
+
+
+
+    useEffect(() => {
+        dispatch(getUserFriends(currentUser.id))
+        dispatch(getUserServers(currentUser.id))
+    }, [dispatch, currentUser])
+
+    useEffect(() => {
+        socket = io();
+
+        // when component unmounts, disconnect
+        return (() => socket.disconnect())
+    }, [dispatch, currentUser])
+
+
+    if (!currentUser || !userFriends || !userServers) {
+        return (
+            <div className='loading-animation'>
+                <div className="center">
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                    <div className="wave"></div>
+                </div>
+            </div>
+        )
+    }
+
+    const serverArr = Object.values(userServers);
+    const privateServerArr = serverArr.filter(server => server.status === true);
+
+    // Starts or reopens a DM if previously opened
+    const handleDM = async (friend) => {
+
+        const friendServers = await dispatch(getFriendServers(friend.id));
+
+        if (friendServers) {
+
+            const friendServerArr = Object.values(friendServers);
+            const friendPrivateServers = friendServerArr.filter(server => server.status === true);
+
+            const index = friend.username.indexOf("#");
+            const slicedFriendUsername = friend.username.slice(0, index);
+
+            const index2 = currentUser.username.indexOf('#')
+            const sessionUsername = currentUser.username.slice(0, index2);
+
+            const privateServerName = `${slicedFriendUsername}-${sessionUsername}`;
+
+            // Check if there is an existing server with the current user and the friend as the members
+            for (let server of privateServerArr) {
+                const members = server.members.map(member => member.id);
+                if (members.includes(currentUser.id) && members.includes(friend.id)) {
+                    return history.push(`/private-messages/${server.id}/${server.channels[0].id}`);
+                }
+            }
+
+            for (let server of friendPrivateServers) {
+                const members = server.members.map(member => member.id);
+                if (members.includes(currentUser.id) && members.includes(friend.id)) {
+                    return history.push(`/private-messages/${server.id}/${server.channels[0].id}`);
+                }
+            }
+
+            await dispatch(addPrivateServer(`${privateServerName}`, currentUser.id, true, currentUser.username, friend.username))
+                .then((res) => {
+                    socket.emit('newServer', res)
+                    history.push(`/private-messages/${res.id}/${res.channels[0].id}`)
+                })
+        }
     }
 
     const handleDelete = async (e, member) => {
@@ -24,36 +113,46 @@ const ServerMembersSidebar = () => {
             })
     }
 
+    //opens the add server member component
+    const openModal = (server) => {
+        setModalContent(<ServerMemberAdd server={server} />);
+    };
+
 
     return (
-        <div className="sidebar">
+        <div className="member-sidebar">
             <h1 className='create-server-header'>Members in {server.name}!</h1>
-            {server.owner_id === user.id ?
-                <div className='server-members-setting-btn'>
-                    <OpenModalButton
-                        buttonText={<i className="fa-solid fa-user-plus"></i>}
-                        modalComponent={<ServerMemberAdd server={server} />}
-                    />
+            <p className='create-server-para'>The following users are members of {server.name}:</p>
+            {server.owner_id === currentUser.id && (
+                <div className='server-members-setting-btn' onClick={() => openModal(server)}>
+                    <div className="server-member-btn">
+                        <i className="fa-solid fa-user-plus"></i>
+                        <span className="add-member-txt">Add a new server member</span>
+                    </div>
                 </div>
-                :
-                ""
-            }
-            <p className='create-server-para'>Check out which cool cats are hanging out here!</p>
+            )}
             <div className='server-members-list-container'>
                 {members.map(member => (
                     <div className='server-member-list-card'>
                         <div className='friendslist-user-container' id="server-members-container" key={`member${member.id}`}>
                             <div className='friendslist-pic-username'>
-                                <div> <img className='friendslist-profile-image' src={member.prof_pic} alt='profile_pic_user' /> </div>
-                                <div className='friendslist-username'> {member.username.split("#")[0]} </div>
-                                <div className='friendslist-tag'> #{member.username.split("#")[1]} </div>
+                                <div>
+                                    <img className='friendslist-profile-image members' src={member.prof_pic ? member.prof_pic : logo} alt='profile_pic_user' />
+                                </div>
+                                <div className='friendslist-username'>
+                                    {member.username.split("#")[0]}
+                                </div>
+                                <div className='friendslist-tag'>
+                                    #{member.username.split("#")[1]}
+                                </div>
                             </div>
                             <div className='friendslist-chat-icon'>
                                 <div className='icon-hover' onClick={handleDM}> <i className="fa-solid fa-message" /> </div>
-                                {server.owner_id === user.id && member.id !== server.owner_id ?
-                                    <div className='icon-hover' onClick={(e) => handleDelete(e, member)}>  <i className="fa-solid fa-ban"></i></div>
-                                    : ""
-                                }
+                                {server.owner_id === currentUser.id && member.id !== server.owner_id && (
+                                    <div className='icon-hover' onClick={(e) => handleDelete(e, member)}>
+                                        <i className="fa-solid fa-ban"></i>
+                                    </div>
+                                )}
                             </div>
                         </div>
                     </div>
